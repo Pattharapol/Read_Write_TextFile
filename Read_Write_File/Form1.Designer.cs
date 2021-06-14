a78cac56b40ba58ab9e518212597bc5623d14973
unknown <tjtik83@gmail.com>
2021-06-14 09:14:02 +0700

    completed

---
+++ Read_Write_File/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Read_Write_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtDB = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUSer = new System.Windows.Forms.TextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(192, 58);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(240, 29);
+            this.txtServer.TabIndex = 0;
+            // 
+            // txtDB
+            // 
+            this.txtDB.Location = new System.Drawing.Point(192, 163);
+            this.txtDB.Name = "txtDB";
+            this.txtDB.Size = new System.Drawing.Size(240, 29);
+            this.txtDB.TabIndex = 1;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(192, 128);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(240, 29);
+            this.txtPass.TabIndex = 2;
+            // 
+            // txtUSer
+            // 
+            this.txtUSer.Location = new System.Drawing.Point(192, 93);
+            this.txtUSer.Name = "txtUSer";
+            this.txtUSer.Size = new System.Drawing.Size(240, 29);
+            this.txtUSer.TabIndex = 3;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(192, 208);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(117, 44);
+            this.btnWrite.TabIndex = 4;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(315, 208);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(117, 44);
+            this.btnRead.TabIndex = 5;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "user";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "database";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(745, 440);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.txtUSer);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtDB);
+            this.Controls.Add(this.txtServer);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtDB;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUSer;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Read_Write_File/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Read_Write_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            string filename = @"C:\Temp\config.txt";
+            StreamWriter sw = new StreamWriter(filename);
+            sw.WriteLine(txtServer.Text);
+            sw.WriteLine(txtUSer.Text);
+            sw.WriteLine(txtPass.Text);
+            sw.WriteLine(txtDB.Text);
+            sw.Close();
+            Process.Start(@"C:\Temp\config.txt");
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string filename = @"C:\Temp\config.txt";
+            string[] lines = File.ReadAllLines(filename, Encoding.UTF8);
+            txtServer.Text = lines[0];
+            txtUSer.Text = lines[1];
+            txtPass.Text = lines[2];
+            txtDB.Text = lines[3];
+        }
+    }
+}
